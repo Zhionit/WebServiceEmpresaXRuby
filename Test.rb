2688f7fac49dd7f0421d9a179145db279386a26a
Marlon D Ramírez - Zhionit <zhionit@live.com>
2016-11-19 00:18:11 -0500

    Modify Test
    Add some usability to console test

--- Test.rb
+++ Test.rb
@@ -11,7 +11,7 @@ class Test
 
       def self.startProgram
             #@swEmpresa = SWEmpresa.new
-            puts( 'before')
+            #puts( 'before')
             SWEmpresa.calcularNomina
       end
 
@@ -25,15 +25,73 @@ puts('Por favor seleccione una opcion:
 5. Un servicio que retorne un empleado especifico de la empresa (buscado por id).
 6. Un servicio que permita modificar el salario de un empleado.
 7. Un servicio que retorne adicione un empleado a la empresa.')
+@WSempresa = SWEmpresa.new
+@WSempresa.addEmployers
 
 loop do
       input = gets.chomp
       command, *params = input.split /\s/
+
       case command
             when '1'
-                  puts( 'before')
-                  @WSempresa = SWEmpresa.new
-                   @WSempresa.calcularNomina
+                  #Calcular nomina
+                  totalNomina = @WSempresa.calcularNomina
+                  puts "Nomina total de la empresa: $#{totalNomina}"
+
+            when '2'
+                  #Calcular número de empleados activos
+                  totalEmpleadosActivos = @WSempresa.calcularEmpleadosActivos
+                  puts "Total Empleados activos de la empresa: #{totalEmpleadosActivos}"
+            when '3'
+                  #Calcular numero de empleados según un estado en particular
+                  puts 'Igrese Estado: '
+                  input = gets.chomp
+                  commandTemp, *params = input
+                  totalEmpleadosSegunEstado = @WSempresa.calcularEmpleadosSegunEstado commandTemp
+                  puts "Total Empleados segun estado: #{totalEmpleadosSegunEstado}"
+            when '4'
+                  #Arreglo con los empleados de la empresa
+                  resultArray = @WSempresa.listaEmpleados
+
+                  for i in 0... resultArray.size
+                        empleado = resultArray.at(i)
+                        puts empleado.id.to_s + ', ' + empleado.nombre + ', ' + ', ' + empleado.fechaNacimiento.to_s + ', ' + empleado.salarioBase.to_s + ', ' + empleado.estado
+                  end
+
+                  #puts resultArray
+                  puts resultArray.size
+            when '5'
+                  #Busqueda de Empleado por Id
+                  puts 'Ingrese el código del empleado: '
+                  input = gets.chomp
+                  commandTemp, *params = input.split /\s/
+                  empleado = @WSempresa.buscarEmpleadoPorId commandTemp
+                  if empleado.nil?
+                        puts 'Empleado no encontrado'
+
+                  else
+                        puts puts empleado.id.to_s + ', ' + empleado.nombre + ', ' + ', ' + empleado.fechaNacimiento.to_s + ', ' + empleado.salarioBase.to_s + ', ' + empleado.estado
+                  end
+            when '6'
+                  #Modificar un salario en específico
+                  puts 'Ingrese el código del empleado: '
+                  input = gets.chomp
+                  idEmpleado, *params = input.split /\s/
+
+                  puts 'Ingrese el nuevo salario: '
+                  input = gets.chomp
+                  nuevoSalario, *params = input.split /\s/
+
+                  empleadoBuscado = @WSempresa.buscarEmpleadoPorId idEmpleado
+                  puts "Anterior Salario: #{empleadoBuscado.salarioBase}"
+                  @WSempresa.cambiarSalarioEmpleado nuevoSalario, idEmpleado
+                  puts "Anterior Salario: #{empleadoBuscado.salarioBase}, nuevo salario: #{nuevoSalario}"
+
+            when '7'
+                  #Adicionar empleado
+                  nuevoEmpleado = Empleado.new 99, 'Nombre 99', Date.new(1914,04,1), 134678, 'No Activo'
+                  @WSempresa.agregarNuevoEmpleado nuevoEmpleado
+                  puts @WSempresa.cantidadEmpleados
             else
                   puts 'Invalid command'
       end
