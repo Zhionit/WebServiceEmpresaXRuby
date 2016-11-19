75c65e17258ed6763f393739ecb0dac62984dbab
Marlon D Ramírez - Zhionit <zhionit@live.com>
2016-11-19 00:55:41 -0500

    Mosify Empresa
    The array is now a global variable or a "static variable" letting add
    new data and keeping alive.
    The algorithm to cambiarSalarioEmpleado has been modified
    The algorithm to agregarNuevoEmpleado has been updated

--- Empresa.rb
+++ Empresa.rb
@@ -8,12 +8,12 @@ require_relative 'Empleado'
 class Empresa
 
 #@@employers
+@@employers = Array.new
       #Constructor
       def initialize(nit, nombre)
             @nit = nit
             @nombre = nombre
-            @employers = Array.new
-            addEmployers
+            #addEmployers
       end
 
       #Accesors
@@ -24,33 +24,33 @@ class Empresa
       #Methods
       def addEmployers
 
-            @employers << Empleado.new(1, "Nombre 1", Date.new(1994,07,19), 9300000, 'Activo')
-            @employers << Empleado.new(2, "Nombre 2", Date.new(1995,05,22), 3240000, 'No Activo')
-            @employers << Empleado.new(3, "Nombre 3", Date.new(1996,06,24), 3110000, 'Activo')
-            @employers << Empleado.new(4, "Nombre 4", Date.new(1997,07,9), 3050010, 'Activo')
-            @employers << Empleado.new(5, "Nombre 5", Date.new(1998,07,7), 3070009, 'No Activo')
-            @employers << Empleado.new(6, "Nombre 6", Date.new(1999,04,14), 1300000, 'Activo')
-            @employers << Empleado.new(7, "Nombre 7", Date.new(1999,03,26), 2000500, 'Activo')
-            @employers << Empleado.new(8, "Nombre 8", Date.new(1999,04,21), 5003000, 'No Activo')
-            @employers << Empleado.new(9, "Nombre 9", Date.new(1993,02,19), 1102000, 'Activo')
-            @employers << Empleado.new(10, "Nombre 10", Date.new(1914,04,1), 3380000, 'Activo')
-
+            @@employers << Empleado.new(1, "Nombre 1", Date.new(1994,07,19), 9300000, 'Activo')
+            @@employers << Empleado.new(2, "Nombre 2", Date.new(1995,05,22), 3240000, 'No Activo')
+            @@employers << Empleado.new(3, "Nombre 3", Date.new(1996,06,24), 3110000, 'Activo')
+            @@employers << Empleado.new(4, "Nombre 4", Date.new(1997,07,9), 3050010, 'Activo')
+            @@employers << Empleado.new(5, "Nombre 5", Date.new(1998,07,7), 3070009, 'No Activo')
+            @@employers << Empleado.new(6, "Nombre 6", Date.new(1999,04,14), 1300000, 'Activo')
+            @@employers << Empleado.new(7, "Nombre 7", Date.new(1999,03,26), 2000500, 'Activo')
+            @@employers << Empleado.new(8, "Nombre 8", Date.new(1999,04,21), 5003000, 'No Activo')
+            @@employers << Empleado.new(9, "Nombre 9", Date.new(1993,02,19), 1102000, 'Activo')
+            @@employers << Empleado.new(10, "Nombre 10", Date.new(1914,04,1), 3380000, 'Activo')
+            @@employers << Empleado.new(11, "Nombre 11", Date.new(1914,04,1), 3380000, 'Activo')
       end
 
       def calcularNomina
             totalNominaEmpresa = 0
             for i in 0...10
-                  totalNominaEmpresa += @employers.at(i).calcularSalario
-                  puts "#{i} Salario Base: #{totalNominaEmpresa}"
+                  totalNominaEmpresa += @@employers.at(i).calcularSalario
+                  #puts "#{i} Salario Base: #{totalNominaEmpresa}"
             end
-            puts('Hello from Empresa, from calcularNomina')
+            #puts('Hello from Empresa, from calcularNomina')
             return totalNominaEmpresa
       end
 
       def calcularEmpleadosActivos
             cantidadActivos = 0
-            for i in 0...@employers.size
-                  if @employers.at(i).estado.eql?("Activo")
+            for i in 0...@@employers.size
+                  if @@employers.at(i).estado.eql?("Activo")
                         cantidadActivos += 1
                   end
             end
@@ -59,8 +59,8 @@ class Empresa
 
       def calcularEmpleadosSegunEstado estado
             cantidadEmpleadosSegunEstado = 0
-            for i in 0...@employers.size
-                  if @employers.at(i).estado.eql?(estado)
+            for i in 0...@@employers.size
+                  if @@employers.at(i).estado.casecmp(estado).zero?
                         cantidadEmpleadosSegunEstado += 1
                   end
             end
@@ -68,15 +68,16 @@ class Empresa
       end
 
       def listaEmpleados
-            @employers
+            @@employers
       end
 
       def buscarEmpleadoPorId idP
-            empleadoBuscado = 0
-
-            for i in 0...@employers.size
-                  if @employers.at(i).id.eql?(idP)
-                        empleadoBuscado = @employers.at(i)
+            empleadoBuscado = nil
+            #puts "employers size: #{@employers.size}"
+            for i in 0...@@employers.size
+#                  puts i
+                  if @@employers.at(i).id.to_s.eql?(idP.to_s)
+                        empleadoBuscado = @@employers.at(i)
                   end
             end
 
@@ -84,20 +85,48 @@ class Empresa
       end
 
       def cambiarSalarioEmpleado nuevoSalario, idP
+
+            @@employers.map! { |element|
+                  if element.id.to_s.eql?(idP.to_s)
+                        #Empleado.new(element.id, element.nombre, element.fechaNacimiento, nuevoSalario, element.estado)
+                        element.salarioBase = nuevoSalario
+                        #puts 'ZZZZ ' + element.salarioBase.to_s
+                  else
+                        element
+                  end
+            }
+=begin
             for i in 0...@employers.size
-                  if @employers.at(i).id.eql?(idP)
+                  if @employers.at(i).id.to_s.eql?(idP.to_s)
                         @employers.at(i).salarioBase = nuevoSalario
+                        puts '---- ' + @employers.at(i).salarioBase.to_s
                   end
             end
+=end
       end
 
       def agregarNuevoEmpleado nuevoEmpleado
-            for i in 0...@employers.size
-                  if @employers.at(i).id.eql?(nuevoEmpleado.id)
-                        puts 'El empleado ya existe'
-                  else
-                        @employers << Empleado.new(nuevoEmpleado.id, nuevoEmpleado.nombre, nuevoEmpleado.fechaNacimiento, nuevoEmpleado.salarioBase, nuevoEmpleado.estado)
+            existe = false
+            for i in 0...@@employers.size do
+                  if @@employers.at(i).id.to_s.eql?(nuevoEmpleado.id.to_s)
+                        existe = true
                   end
             end
+            if !existe
+                  for i in 0...@@employers.size
+                        if @@employers.at(i).id.to_s.eql?(nuevoEmpleado.id.to_s)
+                              puts 'El empleado ya existe'
+                        else
+                              @@employers << Empleado.new(nuevoEmpleado.id, nuevoEmpleado.nombre, nuevoEmpleado.fechaNacimiento, nuevoEmpleado.salarioBase, nuevoEmpleado.estado)
+                              break
+                        end
+                  end
+            else
+                  puts 'El empleado ya existe'
+            end
+      end
+
+      def cantidadEmpleados
+            @@employers.size
       end
 end
